47aba4544023204e8bbd279ac273795d1c2c3569
vobao9898 <vobao9898@gmail.com>
2021-12-12 20:55:18 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Slide from "./component/slide/index";
 const data = [
@@ -63,29 +63,65 @@ const data = [
     image: "https://static.chotot.com/storage/chotot-icons/png/jobtype/2.png",
   },
 ];
-function NextArrow({ onClick }: any) {
+function NextArrow({ onClick, isIconNext }: any) {
   return (
-    <div className="arrow next" onClick={onClick}>
+    <div
+      className={`arrow next ${isIconNext ? "block-arrow" : ""}`}
+      onClick={onClick}
+    >
       <i className="fa fa-chevron-right"></i>,
     </div>
   );
 }
-function PrevArrow({ onClick }: any) {
+function PrevArrow({ onClick, isIconPrev }: any) {
   return (
-    <div className="arrow prev" onClick={onClick}>
+    <div
+      onClick={onClick}
+      className={`arrow prev ${isIconPrev ? "block-arrow" : ""}`}
+    >
       <i className="fa fa-chevron-left"></i>,
     </div>
   );
 }
 function App() {
+  const [indexSlide, setIndexSlide] = useState(0);
+  const [isIconPrev, setIsIconPrev] = useState(true);
+  const [isIconNext, setIsIconNext] = useState(false);
+  const [isScreen, setisScreen] = useState(false);
+  useEffect(() => {
+    if (window.screen.availWidth < 740) {
+      setisScreen(true);
+    }
+  }, [window.screen.width]);
+  function beforeChange(current: number, next: number) {
+    setIndexSlide(next);
+    if (next === 3) {
+      setIsIconNext(true);
+    } else {
+      setIsIconNext(false);
+    }
+    if (current === 0) {
+      setIsIconPrev(true);
+    } else {
+      setIsIconPrev(false);
+    }
+    if (window.screen.availWidth < 740) {
+      setIsIconNext(true);
+      setIsIconPrev(true);
+    }
+  }
+
   return (
     <div className="App">
       <div className="container">
         <Slide
           data={data}
           dots={false}
-          nextArrow={<NextArrow></NextArrow>}
-          prevArrow={<PrevArrow></PrevArrow>}
+          nextArrow={<NextArrow isIconNext={isIconNext}></NextArrow>}
+          prevArrow={<PrevArrow isIconPrev={isIconPrev}></PrevArrow>}
+          infinite={false}
+          indexSlide={indexSlide}
+          beforeChange={(current, next) => beforeChange(current, next)}
         ></Slide>
       </div>
     </div>
--- src/component/slide/index.tsx
+++ src/component/slide/index.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import Slider from "react-slick";
 import "./index.css";
 
@@ -8,25 +8,39 @@ export interface IProps {
   nextArrow?: any;
   prevArrow?: any;
   speed?: number;
+  infinite?: boolean;
+  indexSlide?: number;
+  beforeChange: (c: number, n: number) => void;
+  isScreen?: boolean;
 }
 
 function Slide(props: IProps) {
-  const { data, dots, nextArrow, prevArrow, speed } = props;
+  const {
+    data,
+    dots,
+    nextArrow,
+    prevArrow,
+    speed,
+    isScreen,
+    infinite,
+    indexSlide,
+  } = props;
   const settings = {
     dots: dots,
-    infinite: true,
+    infinite: infinite,
     speed: speed ? speed : 500,
     slidesToShow: 2,
     slidesToScroll: 1,
     nextArrow: nextArrow,
     prevArrow: prevArrow,
   };
+
   function renderContent(datas: Array<any>) {
     let html = null;
     if (datas.length > 0) {
       let dataT = datas as Array<any>;
       html = dataT.map((item, index, array) => {
-        if (index % 2 === 0 && index + 1 < dataT.length) {
+        if (isScreen ? index % 2 === 0 : 1 === 1 && index + 1 < dataT.length) {
           return (
             <div key={`slide-${index}`} className="slide-item">
               <div className="slide-item__one">
@@ -44,23 +58,28 @@ function Slide(props: IProps) {
                   </div>
                 </div>
               </div>
-              <div className="slide-item__one">
-                <div className="slide-item__left">
-                  <img alt="" src={array[index + 1].image} />
+              {isScreen ? null : (
+                <div className="slide-item__one">
+                  <div className="slide-item__left">
+                    <img alt="" src={array[index + 1].image} />
+                  </div>
+                  <div className="slide-item-rigth">
+                    <div className="slide-content__title">
+                      {array[index + 1].name}
+                    </div>
+                    <div className="slide-content__wage">
+                      {array[index + 1].wage}
+                    </div>
+                    <div className="slide-content__address">
+                      <i
+                        className="fa fa-location-arrow"
+                        aria-hidden="true"
+                      ></i>
+                      {array[index + 1].address}
+                    </div>
+                  </div>
                 </div>
-                <div className="slide-item-rigth">
-                  <div className="slide-content__title">
-                    {array[index + 1].name}
-                  </div>
-                  <div className="slide-content__wage">
-                    {array[index + 1].wage}
-                  </div>
-                  <div className="slide-content__address">
-                    <i className="fa fa-location-arrow" aria-hidden="true"></i>
-                    {array[index + 1].address}
-                  </div>
-                </div>
-              </div>
+              )}
             </div>
           );
         } else {
@@ -71,9 +90,9 @@ function Slide(props: IProps) {
     return html;
   }
   return (
-    <div>
-      <Slider {...settings}>{renderContent(data as Array<any>)}</Slider>
-    </div>
+    <Slider {...settings} beforeChange={(c, n) => props.beforeChange(c, n)}>
+      {renderContent(data as Array<any>)}
+    </Slider>
   );
 }
 
